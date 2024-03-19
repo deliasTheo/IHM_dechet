a9fe248823093175109d6f148fc5b9c1b8ca9688
Mathis MAGNIN <mathis.magnin1@etu.univ-cotedazur.fr>
2024-03-19 15:51:43 +0100

    merge

--- app/src/main/java/edu/polytech/ihmtd2dechet/MapActivity.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/MapActivity.java
@@ -4,15 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<< HEAD:app/src/main/java/edu/polytech/ihmtd2dechet/MainActivity.java
-public class MainActivity extends AppCompatActivity {
-=======
 public class MapActivity extends AppCompatActivity {
 
->>>>>>> e5d3542ea90c3c53f554e966f44cbd8d9db663df:app/src/main/java/edu/polytech/ihmtd2dechet/MapActivity.java
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
     }
+
 }
