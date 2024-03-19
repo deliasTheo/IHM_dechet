408f2ed22740e5ec9a78863c0ef4f04475edf380
Mathis MAGNIN <mathis.magnin1@etu.univ-cotedazur.fr>
2024-03-19 15:45:01 +0100

    activity list

--- app/src/main/java/edu/polytech/ihmtd2dechet/ListActivity.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/ListActivity.java
@@ -1,13 +1,15 @@
 package edu.polytech.ihmtd2dechet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListActivity extends AppCompatActivity  {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
     }
+
 }
