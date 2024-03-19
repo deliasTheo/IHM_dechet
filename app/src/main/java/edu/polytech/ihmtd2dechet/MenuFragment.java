219f1f2c95b90c15af918ea3295f9c74ffc242a6
deliasTheo <81029828+deliasTheo@users.noreply.github.com>
2024-03-19 17:13:02 +0100

    pin invisible when clicked

--- app/src/main/java/edu/polytech/ihmtd2dechet/MenuFragment.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/MenuFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.app.Fragment;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MenuFragment extends Fragment {
     public MenuFragment() { } //constructeur (même vide)
@@ -13,7 +15,12 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
         //do something
-
+        ImageView logoMap = rootView.findViewById(R.id.mapPin);
+        logoMap.setOnClickListener(click -> {
+            if(logoMap.getVisibility()==View.VISIBLE){
+                logoMap.setVisibility(View.INVISIBLE);
+            }
+        });
         return rootView;
     }
 }
