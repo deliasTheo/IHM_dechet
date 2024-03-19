1f71d96599293f7bea71876583021fb90f1aebbd
deliasTheo <81029828+deliasTheo@users.noreply.github.com>
2024-03-20 00:13:47 +0100

    link establish with the map activity

--- app/src/main/java/edu/polytech/ihmtd2dechet/MenuFragment.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/MenuFragment.java
@@ -1,25 +1,24 @@
 package edu.polytech.ihmtd2dechet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.app.Fragment;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MenuFragment extends Fragment {
-    public MenuFragment() { } //constructeur (même vide)
+    public MenuFragment() { }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
         //do something
         ImageView logoMap = rootView.findViewById(R.id.mapPin);
         logoMap.setOnClickListener(click -> {
-            if(logoMap.getVisibility()==View.VISIBLE){
-                logoMap.setVisibility(View.INVISIBLE);
-            }
+            Intent intent = new Intent(getContext(), MapActivity.class);
+            startActivity(intent);
         });
         return rootView;
     }
