9b84cc89f9566da601b46121d24f31755f72f97f
Sara-tfq <147254110+Sara-tfq@users.noreply.github.com>
2024-03-26 03:54:36 +0100

    Ajout du bouton de navigation vers le guide #7

--- app/src/main/java/edu/polytech/ihmtd2dechet/GuideActivity.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/GuideActivity.java
@@ -1,5 +1,7 @@
 package edu.polytech.ihmtd2dechet;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,15 @@ public class GuideActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fragment fragmentMenu= new MenuFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_menu, fragmentMenu);
+        transaction.commit();
+
+
         setContentView(R.layout.activity_guide);
 
-    }
 
 
-
-}
+}}
