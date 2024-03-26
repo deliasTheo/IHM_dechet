4aa8a2dd742d49480ba7a4fbbb73822a1a8d7c2e
Sara-tfq <147254110+Sara-tfq@users.noreply.github.com>
2024-03-26 03:01:54 +0100

    Ajout du bouton de navigation vers le guide #7

--- app/src/main/java/edu/polytech/ihmtd2dechet/GuideActivity.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/GuideActivity.java
@@ -3,6 +3,7 @@ package edu.polytech.ihmtd2dechet;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class GuideActivity extends AppCompatActivity  {
 
@@ -10,6 +11,16 @@ public class GuideActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+
+        MenuFragment menuFragment = new MenuFragment();
+
+       
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_menu, menuFragment)
+                .commit();
     }
+
+
 
 }
--- app/src/main/java/edu/polytech/ihmtd2dechet/MenuFragment.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/MenuFragment.java
@@ -1,19 +1,32 @@
 package edu.polytech.ihmtd2dechet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.app.Fragment;
+import android.widget.ImageView;
 
 public class MenuFragment extends Fragment {
     public MenuFragment() { } //constructeur (même vide)
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
-        //do something
+
+        ImageView logoMap = rootView.findViewById(R.id.mapPin);
+        logoMap.setOnClickListener(view -> navigateToActivity(MapActivity.class));
+
+        ImageView logoGuide = rootView.findViewById(R.id.guideIcon);
+        logoGuide.setOnClickListener(view -> navigateToActivity(GuideActivity.class));
 
         return rootView;
+    }
+
+    private void navigateToActivity(Class<?> activityClass) {
+        Intent intent = new Intent(getContext(), activityClass);
+        startActivity(intent);
     }
 }
