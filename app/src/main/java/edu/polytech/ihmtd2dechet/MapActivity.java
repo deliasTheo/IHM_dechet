6bc7eeb125eea449b94ffd8ff96fc7ed3e9c0f7e
deliasTheo <81029828+deliasTheo@users.noreply.github.com>
2024-04-02 14:47:09 +0200

    add mapView

--- app/src/main/java/edu/polytech/ihmtd2dechet/MapActivity.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/MapActivity.java
@@ -1,18 +1,34 @@
 package edu.polytech.ihmtd2dechet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 
 import android.app.Fragment;
 
 import android.app.FragmentTransaction;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
 public class MapActivity extends AppCompatActivity  {
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
 
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
@@ -21,6 +37,48 @@ public class MapActivity extends AppCompatActivity  {
         transaction.commit();
 
         setContentView(R.layout.activity_map);
+
+        map = findViewById(R.id.mapView);
+        map.setTileSource(TileSourceFactory.MAPNIK);    // render
+        map.setBuiltInZoomControls(true);               // roomable
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+        IMapController mapController = map.getController();
+        mapController.setZoom(18.0);
+        mapController.setCenter(startPoint);
+
+        ArrayList<OverlayItem> items= new ArrayList<>();
+        OverlayItem home = new OverlayItem("Theo's office", "my office", new GeoPoint(43.65020, 7.00517));
+        Drawable m = home.getMarker(0);
+        items.add(home);
+        items.add(new OverlayItem("Resto", "chez babar", new GeoPoint(43.64950, 7.00517)));
+
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(),
+                items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
+
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        map.onResume();
     }
 
 }
