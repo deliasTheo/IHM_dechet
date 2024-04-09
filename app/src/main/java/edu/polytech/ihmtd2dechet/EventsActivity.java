4024a4e7e1f14e3a5865e34a5fba6fd128ce8c8e
MaxenceRoques <maxence.roques@etu.univ-cotedazur.fr>
2024-04-09 16:01:48 +0200

    Event

--- app/src/main/java/edu/polytech/ihmtd2dechet/EventsActivity.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/EventsActivity.java
@@ -1,10 +1,18 @@
 package edu.polytech.ihmtd2dechet;
 
+import static edu.polytech.ihmtd2dechet.ApplicationEvenement.VALUE_TITRE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventsActivity extends AppCompatActivity {
 
@@ -16,7 +24,13 @@ public class EventsActivity extends AppCompatActivity {
         Fragment fragmentMenu = new MenuFragment();
         transaction.replace(R.id.fragment_menu, fragmentMenu);
         transaction.commit();
-
         setContentView(R.layout.activity_events);
+
+        ((Button)findViewById(R.id.button_créer_evenement)).setOnClickListener(click -> {
+            Intent intent = new Intent(getApplicationContext(), CreateEventActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
---
+++ app/src/main/java/edu/polytech/ihmtd2dechet/OneEventActivity.java
@@ -0,0 +1,14 @@
+package edu.polytech.ihmtd2dechet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OneEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_event);
+    }
+}
