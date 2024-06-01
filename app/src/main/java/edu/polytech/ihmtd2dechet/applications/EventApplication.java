f738c7142154b8fbe95cf69494f651606e5d82c1
MaxenceRoques <maxence.roques@etu.univ-cotedazur.fr>
2024-06-01 16:22:31 +0200

    import calandrier

--- app/src/main/java/edu/polytech/ihmtd2dechet/applications/EventApplication.java
+++ app/src/main/java/edu/polytech/ihmtd2dechet/applications/EventApplication.java
@@ -5,6 +5,8 @@ public abstract class EventApplication {
     public static String VALUE_DESCRIPTION = "description value";
     public static String VALUE_DATE = "date value";
     public static String VALUE_LOCATION = "location value";
+    public static String VALUE_START_TIME = "start time value";
+    public static String VALUE_END_TIME = "end time value";
     public static int VALUE_IMAGE;
 
     public static final String EVENT = "event";
